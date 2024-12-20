8ccb9d521fa464f0baed69b0a8df0a2950b37664
Lorenzo Bonanni <95477404+ebona1000@users.noreply.github.com>
2024-12-20 16:30:53 +0000

    hello.c

---
+++ hello/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello.word\n\n ciao");
+    char name [32];
+    printf("Enter a name");
+
+    // read the imput of the user 
+    scanf("%s", name);
+    printf ("hello\t %s", name);
+    return 0;
+}
